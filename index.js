f11d4008e3058cbaa8601d4dcf1de21031cba6d7
Credit <44134254+alpacca4@users.noreply.github.com>
2021-10-08 14:19:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
+
 const app =  express()
 
+function arvoNumero(alaraja, ylaraja) {
+    return Math.floor(ylaraja * Math.random()) + alaraja
+}
+
+let NUMEROLISTA = [
+        arvonumero(0.5)
+        arvonumero(10.15)
+        arvonumero(100.500)
+        arvonumero(10.20)
+        arvonumero(0.5)
+      ]
 app.get('/',function (req, res) {
-  res.send('1, 5, 8, 2, 5,')
+  res.send(NUMEROLISTA)
 })
 
 app.listen(PORT)
