13b15b4ee663ba62ea1bb140e69e22467ecb90ed
Credit <44134254+alpacca4@users.noreply.github.com>
2021-10-08 13:48:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-
+const PORT = process.env.PORT || 5000
 const app =  express()
 
-app.get('/',
-function (req, res) {
+app.get('/',function (req, res) {
   res.send('1 5 8 2 5')
 })
 
-app.listen(3000)
+app.listen(PORT)
